2ea4383987a8248808358906183481be20eb1ae3
lc-101010 <lcommander101010@gmail.com>
2024-11-10 07:56:17 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="flex flex-col bg-ctp-base">
+      <h1 className="font-bold text-3xl text-ctp-maroon">Food App</h1>
+    </div>
+  );
+};
+
+export default App;
